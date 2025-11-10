f37cdab0c0aa1fb056b8951fd133bd97e8a2c097
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-10 17:09:29 +0530

    chore: added redux store and authSlice

---
+++ src/reduxStore/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoggedIn : !!localStorage.getItem("token"),
+    token : localStorage.getItem("token") || null,
+    userEmail:localStorage.getItem("email") || null,
+};
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login(state, action){
+            state.isLoggedIn = true;
+            state.token = action.payload.token;
+            state.userEmail = action.payload.email;
+            localStorage.setItem("token",action.payload.token);
+            localStorage.setItem("email",action.payload.email);
+        },
+        logout(state){
+            state.isLoggedIn = false;
+            state.token = null;
+            satisfies.userEmail=null;
+            localStorage.clear();
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions;
+export default authSlice.reducer;
