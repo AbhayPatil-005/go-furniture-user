ebf15f775db81014071f40d415d3134697d446f6
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-13 09:24:13 +0530

    feat: Added Product card view for product view

--- src/components/home/ProductCard.jsx
+++ src/components/home/ProductCard.jsx
@@ -1,7 +1,38 @@
+import { Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
+const ProductCard=({product})=>{
+    const navigate = useNavigate();
 
-const ProductCard=()=>{
-    return(<>
+    return (<>
+        <Card
+            className="shadow-sm h-100 product-card"
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(`/product/${product.id}`)}
+            >
+            <Card.Img
+                variant="top"
+                src={product.imageUrl}
+                style={{ height: "220px", objectFit: "cover" }}
+            />
+
+            <Card.Body>
+                <Card.Title className="fs-6">{product.name}</Card.Title>
+
+                <p className="fw-bold text-success mb-2">₹ {product.price}</p>
+
+                <Button
+                    variant="dark"
+                    className="w-100"
+                    onClick={(e) => {
+                        e.stopPropagation(); // prevent card click
+                        console.log("Add to cart:", product.id);
+                    }}
+                >
+                    Add to Cart
+                </Button>
+            </Card.Body>
+        </Card>
     </>)
 }
 export default ProductCard;
