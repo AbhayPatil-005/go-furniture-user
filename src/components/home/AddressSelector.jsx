2698fd184d40ff91b1aa0e5aac828a32bac9f6df
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-13 16:19:43 +0530

    feat: Added Adress Selector component

---
+++ src/components/home/AddressSelector.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import {Button, ListGroup} from "react-bootstrap";
+import AddressForm from "./AddressForm";
+
+const AddressSelector=({addresses, onSelect})=>{
+    const [addingNew, setAddingNew] = useState(false);
+    if(addingNew)
+        return(<AddressForm onSubmit={(addr) => onSelect(addr)} />);
+    return(
+        <>
+            <h5>Select Delivery Address</h5>
+            <ListGroup className="mb-3">
+                {addresses.map((addr, idx) => (
+                    <ListGroup.Item
+                        key={idx}
+                        action
+                        onClick={() => onSelect(addr)}
+                        className="p-3"
+                    >
+                        <strong>{addr.name}</strong>, {addr.street}, {addr.city} - {addr.pincode}
+                        <br />
+                        Phone: {addr.phone}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+
+            <Button variant="outline-primary" onClick={() => setAddingNew(true)}>
+                + Add New Address
+            </Button>
+        </>
+    )
+}
+
+export default AddressSelector;
