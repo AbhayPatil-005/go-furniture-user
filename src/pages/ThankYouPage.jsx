8f99d78a1c8e1e6fbeba83c1fdde9663e7cf7de9
AbhayPatil-005 <abhay.official005@gmail.com>
2025-11-19 22:29:06 +0530

    feat: added Header and Footer to ThankYouPage.jsx

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 const ThankYouPage = () => {
-  return (
+  return (<>
+    <Header />
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <Card className="p-4 text-center shadow-sm" style={{ maxWidth: "420px" }}>
+      <Card className="p-4 text-center shadow-sm" style={{ maxWidth: "420px"}}>
         <h2 className="fw-bold text-success"> Order Placed!</h2>
         <p className="text-muted mt-2">
           Thank you for shopping with <strong>Go-Furniture</strong>.<br />
@@ -30,7 +33,8 @@ const ThankYouPage = () => {
         </Button>
       </Card>
     </div>
-  );
+    <Footer />
+  </>);
 };
 
 export default ThankYouPage;
